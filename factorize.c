ec4a2c3b1e804994836d7332ffe74bfea27adcd4
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-19 00:17:13 +0100

    Update factorize.c

--- factorize.c
+++ factorize.c
@@ -1,1 +1,23 @@
+#include "factor.h"
+/**
+ * factorize - This function factorizes a number
+ * @buffer: pointer to the address of the said number
+ * Return: An int
+ */
+int factorize(char *buffer)
+{
+	u_int32_t num;
+	u_int32_t i;
 
+	num = atoi(buffer);
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+		{
+			printf("%d=%d*%d\n",num,num/i,i);
+			break;
+		}
+	}
+  return (0);
+}
